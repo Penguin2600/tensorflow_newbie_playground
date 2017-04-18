f69dbd8d3e631d983554606d8910cb13385ddfb1
Charles V Bock <charles@charlesbock.com>
2017-04-17 23:20:25 -0700

    stuff

--- tfnewb.py
+++ tfnewb.py
@@ -1,71 +1,102 @@
-tensorboard_file='./tboardfile'
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tensorboard_file='tflogdir'
+save_file='tflogdir/model.ckpt'
+
+# training data
+x_train = [1,2,3,4]
+y_train = [-2,-4,-6,-8]
 
 import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.ERROR)
 
 sess = tf.InteractiveSession()
 
+# The simplest model
 with tf.name_scope('adding_raw'):
-	node1 = tf.constant(3.0, tf.float32)
-	node2 = tf.constant(9.0)
-	print(node1,node2)
+    node1 = tf.constant(3.0, tf.float32)
+    node2 = tf.constant(9.0)
+    print(node1,node2)
 
-	node3 = tf.add(node1, node2)
-	result = sess.run(node3)
-	print(result)
+    node3 = tf.add(node1, node2)
+    result = sess.run(node3)
+    print(result)
 
+# Example of placeholders and arguements
 with tf.name_scope('adding_func'):
-	a = tf.placeholder(tf.float32)
-	b = tf.placeholder(tf.float32)
-	adder_node = a + b 
-	result = sess.run(adder_node, {a: 3, b:4.5})
-	print(result)
+    a = tf.placeholder(tf.float32)
+    b = tf.placeholder(tf.float32)
+    adder_node = a + b 
+    result = sess.run(adder_node, {a: 3, b:4.5})
+    print(result)
 
+# Example of a multi layer model
 with tf.name_scope('add_mult'):
-	a = tf.placeholder(tf.float32)
-	b = tf.placeholder(tf.float32)
-	c = tf.placeholder(tf.float32)
-	adder_node = a + b 
-	add_mult = adder_node * c
-	result = sess.run(add_mult, {a: 3, b:4.5, c:2})
-	print(result)
+    a = tf.placeholder(tf.float32)
+    b = tf.placeholder(tf.float32)
+    c = tf.placeholder(tf.float32)
+    adder_node = a + b 
+    add_mult = adder_node * c
+    result = sess.run(add_mult, {a: 3, b:4.5, c:2})
+    print(result)
 
-
+# Example of a linear model, y=Wx+b 
 with tf.name_scope('linear_model'):
-	W = tf.Variable([.3], tf.float32)
-	b = tf.Variable([-.3], tf.float32)
-	x = tf.placeholder(tf.float32)
-	linear_model = W * x + b
-	init = tf.global_variables_initializer()
-	sess.run(init)
-	result=sess.run(linear_model, {x:[1,2,3,4]})
-	print(result)
+    W = tf.Variable([.1], tf.float32)
+    b = tf.Variable([.3], tf.float32)
+    x = tf.placeholder(tf.float32)
+    linear_model = W * x + b
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    result=sess.run(linear_model, {x:x_train})
+    tf.summary.histogram('linear', linear_model)
+    tf.summary.histogram('W', W)
+    tf.summary.histogram('b', b)
 
+
+    print(result)
+
+# Simple loss calculations
 with tf.name_scope('mean_squared_error'):
-	y = tf.placeholder(tf.float32)
-	squared_deltas = tf.square(linear_model - y)
-	loss = tf.reduce_sum(squared_deltas)
-	error=sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]})
-	print(error)
+    y = tf.placeholder(tf.float32)
+    squared_deltas = tf.square(linear_model - y)
+    loss = tf.reduce_sum(squared_deltas)
+    error=sess.run(loss, {x:x_train, y:y_train})
+    tf.summary.scalar('loss', loss)
+    print(error)
 
 with tf.name_scope('training'):
-	optimizer = tf.train.GradientDescentOptimizer(0.01)
-	train = optimizer.minimize(loss)
-	# training data
-	x_train = [1,2,3,4]
-	y_train = [0,-1,-2,-3]
-	# training loop
-	init = tf.global_variables_initializer()
-	sess = tf.Session()
-	sess.run(init) # reset values to wrong
-	for i in range(1000):
-		sess.run(train, {x:x_train, y:y_train})
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    train = optimizer.minimize(loss)
 
-# evaluate training accuracy
-curr_W, curr_b, curr_loss  = sess.run([W, b, loss], {x:x_train, y:y_train})
-print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+# Setup for tensorboard logging
 merged_summary = tf.summary.merge_all()
 print("Writing for TensorBoard to file %s"%(tensorboard_file))
 writer = tf.summary.FileWriter(tensorboard_file)
 writer.add_graph(sess.graph)
+
+# training loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init) # reset values to wrong
+for i in range(1000):
+    sess.run(train, {x:x_train, y:y_train})
+    s = sess.run(merged_summary, feed_dict={x: x_train, y:y_train})
+    writer.add_summary(s, i)
+    if i%100 == 0:
+        train_error = sess.run(loss, {x:x_train, y:y_train})
+        print("step %d, training error  %g"%(i, train_error))
+        if train_error < 0.00001:
+            break
+
+
+
+# evaluate training accuracy
+curr_W, curr_b, curr_loss  = sess.run([W, b, loss], {x:x_train, y:y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+
+print("Saving neural network to %s.*"%(save_file))
+saver = tf.train.Saver()
+saver.save(sess, save_file,1)
+
 writer.flush()
---
+++ tictac/tictac.py
@@ -0,0 +1,176 @@
+""" 2-input XOR example """
+from __future__ import print_function
+ 
+import os
+import string
+from itertools import cycle
+import random
+from neat import nn, population, statistics
+import time
+ 
+class tictac(object):
+    """class implements tictac"""
+    def __init__(self, net=None, rand=False):
+        super(tictac, self).__init__()
+        self.boards = [[0] * 9, [0] * 9]
+        self.moves_remaining = 9
+        self.players = cycle(['X','O'])
+        self.winner = False
+        self.current_player=next(self.players)
+        self.net = net
+        self.rand=rand
+
+ 
+    def check_vector(self, iterator):
+        # given a segment, are all the positions the same.
+        return bool(bool(len(set(iterator)) == 1) and iterator[0])
+ 
+    def visual(self):
+        rows=[0,3,6]
+        self.moves_remaining = 8
+        for row in rows:
+            ascii_row=""
+            for ind in range(0,3):
+                cell=ind+row
+                newcell=cell
+                for idx, board in enumerate(self.boards):
+                    if board[cell]:
+                        self.moves_remaining -= 1
+                        newcell = 'X' if idx == 1 else 'O'
+
+                ascii_row = "{}[{}]".format(ascii_row,newcell)
+
+            #print(ascii_row)
+
+
+    def itemgetter(self, indicies, iterator):
+        return map(iterator.__getitem__, indicies)
+ 
+    def is_valid_play(self, move):
+        for board in self.boards:
+            if board[move]:
+                return False
+        return True
+ 
+    def check_win(self):
+        for board in self.boards:
+            vectors = [
+                # horizontal wins
+                self.itemgetter([0, 1, 2], board),
+                self.itemgetter([3, 4, 5], board),
+                self.itemgetter([6, 7, 8], board),
+                # vertical wins
+                self.itemgetter([0, 3, 6], board),
+                self.itemgetter([1, 4, 7], board),
+                self.itemgetter([2, 5, 8], board),
+                # diag wins
+                self.itemgetter([0, 4, 8], board),
+                self.itemgetter([2, 4, 6], board)]
+            for vector in vectors:
+                if self.check_vector(vector):
+                    return self.current_player
+        if self.moves_remaining <= 0:
+            return "No One"
+        return False
+
+    def set_move(self, play):
+        if self.current_player == 'X':
+            self.boards[1][play] = 1
+        else:
+            self.boards[0][play] = 1
+
+
+    def doturn(self, play=None):
+        self.current_player = next(self.players)
+        while play==None:
+            if self.net:
+                if self.current_player=='X':
+                    inputs = []
+                    inputs = list(self.boards[0])
+                    inputs.extend(self.boards[1])
+                    votes={}
+                    output = self.net.serial_activate(inputs)
+                    for idx, val in enumerate(output):
+                        votes[idx] = val
+                    votes = sorted(votes, key=votes.get)
+                    for vote in votes:
+                        if self.is_valid_play(vote):
+                            play = vote
+                            self.set_move(play)
+                            #print ("Net plays: {}".format(play))
+                            return
+            if self.rand:
+                if self.current_player=='O':
+                    votes=[0,1,2,3,4,5,6,7,8]
+                    random.shuffle(votes)
+                    for vote in votes:
+                        if self.is_valid_play(vote):
+                            play = vote
+                            self.set_move(play)
+                            #print ("Rnd plays: {}".format(play))
+                            return
+
+
+            play = int(raw_input("{}'s move:".format(self.current_player)))
+            if self.is_valid_play(play):
+                self.set_move(play)
+            else:
+                play = None  
+
+    def start_game(self):
+        while self.winner==False:
+            self.visual()
+            self.doturn()
+            self.winner = self.check_win()
+        #print("Game Over!, {} Wins!".format(self.winner))
+        return self.winner
+
+
+def eval_fitness(genomes):
+    #shall we play a game
+    
+    for g in genomes:
+        sum_error = 0.0
+        for x in range(400):
+            net = nn.create_feed_forward_phenotype(g)
+            game = tictac(net,rand=True)
+            result = game.start_game()
+            if result=='O':
+                fit=0.0
+            if result=='No One':
+                fit=1
+            if result=='X':
+                fit=1
+            sum_error += fit * 0.0025
+            # When the output matches expected for all inputs, fitness will reach
+            # its maximum value of 1.0.
+        #print(1 - sum_square_error)
+        g.fitness = sum_error
+ 
+ 
+local_dir = os.path.dirname(__file__)
+config_path = os.path.join(local_dir, 'ttconfig')
+pop = population.Population(config_path)
+pop.run(eval_fitness, 3000)
+ 
+# Log statistics.
+statistics.save_stats(pop.statistics)
+statistics.save_species_count(pop.statistics)
+statistics.save_species_fitness(pop.statistics)
+ 
+print('Number of evaluations: {0}'.format(pop.total_evaluations))
+ 
+# Show output of the most fit genome against training data.
+winner = pop.statistics.best_genome()
+print('\nBest genome:\n{!s}'.format(winner))
+print('\nOutput:')
+
+winner_net = nn.create_feed_forward_phenotype(winner)
+
+game = tictac(winner_net,rand=False)
+result = game.start_game()
+import pdb; pdb.set_trace()
+import pickle
+with open('winner.pkl', 'wb') as output:
+    pickle.dump(winner, output, 1)
+
