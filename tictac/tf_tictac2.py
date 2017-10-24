630bc2688add0f5faf085d2ed11a3c7a75f27343
Charles V Bock <charles.v.bock@intel.com>
2017-10-24 16:41:01 -0700

    more fixins

--- tictac/tf_tictac2.py
+++ tictac/tf_tictac2.py
@@ -3,13 +3,14 @@ from __future__ import print_function
 
 #Supress TF Spam
 import os
+from copy import copy
 from tictac2 import TicTacGame, Player, Board
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 # Some globals :/ dont hate.
 
-do_training = 0 # 1 = do the training, 0 = load from file and just run it
+do_training = 1 # 1 = do the training, 0 = load from file and just run it
 save_trained = 1 # 1 = save to file after training, 0 = don't save
 # Change the following to where you want the network to be saved to.
 # Make sure to create the directory structure.
@@ -45,11 +46,11 @@ def vote_for(move):
 def play_games_random():
     won_games=[]
     with tf.name_scope('play_game'):
-        for _ in range(ITTERCOUNT):
+        for _ in range(500):
             #generate new game
                 #Do a standard human random game.
-            p1=Player('ai_player', 'random', 1)
-            p2=Player('random_player', 'random', 0)
+            p1=Player('random_player1', 'random', 1)
+            p2=Player('random_player2', 'random', 0)
             board=Board()
             localgame=TicTacGame(board, p1, p2)
             history=[]
@@ -57,25 +58,16 @@ def play_games_random():
             while localgame.winner==None:
                 localgame.current_player = next(localgame.turn_order)
                 localgame.board.do_visual()
-
-                # Get board state as flat vector for the AI
-                inputs = localgame.board.board
-                #print(localgame.board.board)
-                net_plays = sess.run(results, feed_dict={x: [inputs]})
-                max_value = max(net_plays[0])
-                max_index = net_plays[0].tolist().index(max_value)
-                #play whatever the AI weights highest
-                p1.move = max_index
                 is_valid = localgame.current_player.play(localgame.board)
-                #spacial case, punish AI player for making an invalid move.
-                if localgame.current_player.name=='ai_player':
-                  history.append((inputs, localgame.current_player.move))
+                if localgame.current_player.name=='random_player1':
+                  history.append((copy(localgame.board.board), localgame.current_player.move))
                 localgame.winner = localgame.board.check_win()
             if localgame.winner:
                 print("Game Over!, {} Wins!".format(localgame.current_player.name))
-                if localgame.current_player.name == 'ai_player':
+                if localgame.current_player.name == 'random_player1':
                   #take winning games and build training data
                   won_games.append(history)
+                  print(history)
             else:
                 print("Game Over!, No one Wins!")
 
@@ -85,7 +77,6 @@ def play_games_random():
                 inputvals.append(move[0])
                 targetvals.append(vote_for(move[1]))
         print(len(won_games)/float(ITTERCOUNT))
-
 # play a bunch of games and remember the ones we won
 def play_human():
     won_games=[]
@@ -108,7 +99,7 @@ def play_human():
                 net_plays = sess.run(results, feed_dict={x: [inputs]})
                 max_value = max(net_plays[0])
                 max_index = net_plays[0].tolist().index(max_value)
-                #print(net_plays, max_index)
+                print(net_plays, max_index)
                 #play whatever the AI weights highest
                 p1.move = max_index
                 is_valid = localgame.current_player.play(localgame.board)
@@ -140,10 +131,59 @@ def play_games():
     won_games=[]
     with tf.name_scope('play_game'):
         for _ in range(ITTERCOUNT):
+            raw_input("Press Enter to continue...")
             #generate new game
                 #Do a standard human random game.
             p1=Player('ai_player', 'AI', 1)
             p2=Player('random_player', 'random', 0)
+            board=Board()
+            localgame=TicTacGame(board, p1, p2)
+            history=[]
+
+            while localgame.winner==None:
+                localgame.current_player = next(localgame.turn_order)
+                localgame.board.do_visual()
+
+                # Get board state as flat vector for the AI
+                inputs = list(localgame.board.board)
+                net_plays = sess.run(results, feed_dict={x: [inputs]})
+                max_value = max(net_plays[0])
+                max_index = net_plays[0].tolist().index(max_value)
+                print(net_plays, max_index)
+                #play whatever the AI weights highest
+                p1.move = max_index
+                is_valid = localgame.current_player.play(localgame.board)
+                if is_valid == False:
+                  print("Invalid Move!")
+                  break
+                #spacial case, punish AI player for making an invalid move.
+                history.append((inputs, max_index))
+                localgame.winner = localgame.board.check_win()
+            if localgame.winner:
+                print("Game Over!, {} Wins!".format(localgame.current_player.name))
+                if localgame.current_player.name == 'ai_player':
+                  #take winning games and build training data
+                  won_games.append(history)
+            else:
+                print("Game Over!, No one Wins!")
+
+        print("games won:",len(won_games))
+        for game in won_games:
+            for move in game:
+                inputvals.append(move[0])
+                targetvals.append(vote_for(move[1]))
+        print("won percent:", (len(won_games)/float(ITTERCOUNT))*100)
+        #print(won_games, "\n============\n", inputvals, "\n============\n", targetvals)
+
+# play a bunch of games and remember the ones we won
+def play_games_self():
+    won_games=[]
+    with tf.name_scope('play_game'):
+        for _ in range(ITTERCOUNT):
+            #generate new game
+                #Do a standard human random game.
+            p1=Player('ai_player1', 'AI', 1)
+            p2=Player('ai_player2', 'AI', 0)
             board=Board()
             localgame=TicTacGame(board, p1, p2)
             history=[]
@@ -160,20 +200,31 @@ def play_games():
                 #print(net_plays, max_index)
                 #play whatever the AI weights highest
                 p1.move = max_index
+
+                #allow second guess for not improving net
+                is_valid=False
+                while not is_valid:
+                  max_value = max(net_plays[0])
+                  p2.move = net_plays[0].tolist().index(max_value)
+                  is_valid = localgame.board.is_valid_play(p2.move)
+                  if is_valid == False:
+                    del net_plays[0][p2.move]
+
+
                 is_valid = localgame.current_player.play(localgame.board)
                 if is_valid == False:
-                  #print("Invalid Move!")
+                  print("Invalid Move!")
                   break
                 #spacial case, punish AI player for making an invalid move.
                 history.append((inputs, max_index))
                 localgame.winner = localgame.board.check_win()
             if localgame.winner:
-                #print("Game Over!, {} Wins!".format(localgame.current_player.name))
-                if localgame.current_player.name == 'ai_player':
+                print("Game Over!, {} Wins!".format(localgame.current_player.name))
+                if localgame.current_player.name == 'ai_player1':
                   #take winning games and build training data
                   won_games.append(history)
-            #else:
-                #print("Game Over!, No one Wins!")
+            else:
+                print("Game Over!, No one Wins!")
 
         print("games won:",len(won_games))
         for game in won_games:
@@ -246,7 +297,7 @@ if do_training == 1:
   print(len(targetvals))
   play_games_random()
 
-  for i in range(ITTERCOUNT):
+  for i in range(500):
       if i%100 == 0:
         train_error = cross_entropy.eval(feed_dict={x: inputvals, y_:targetvals})
         print("step %d, training error  %g"%(i, train_error))
@@ -257,7 +308,7 @@ if do_training == 1:
 
       sess.run(train_step, feed_dict={x: inputvals, y_: targetvals})
 
-  for _ in range(200):
+  for _ in range(30):
 
     #remember some stuff but let trash fall out
     inputvals = inputvals[-memory:]
@@ -265,7 +316,7 @@ if do_training == 1:
     print('memory depth:', len(targetvals))
     play_games()
 
-    for i in range(ITTERCOUNT):
+    for i in range(500):
         if i%100 == 0:
           train_error = cross_entropy.eval(feed_dict={x: inputvals, y_:targetvals})
           print("step %d, training error  %g"%(i, train_error))
@@ -275,12 +326,32 @@ if do_training == 1:
           writer.add_summary(s, i)
 
         sess.run(train_step, feed_dict={x: inputvals, y_: targetvals})
-
-    if save_trained == 1:
-        print("Saving neural network to %s.*"%(save_file))
-        saver = tf.train.Saver()
-        saver.save(sess, save_file)
     #raw_input("Press Enter to continue...")
+
+  # for _ in range(40):
+
+  #   #remember some stuff but let trash fall out
+  #   inputvals = inputvals[-memory:]
+  #   targetvals = targetvals[-memory:]
+  #   print('memory depth:', len(targetvals))
+  #   play_games_self()
+
+  #   for i in range(500):
+  #       if i%100 == 0:
+  #         train_error = cross_entropy.eval(feed_dict={x: inputvals, y_:targetvals})
+  #         print("step %d, training error  %g"%(i, train_error))
+
+  #       if write_for_tensorboard == 1 and i%5 == 0:
+  #         s = sess.run(merged_summary, feed_dict={x: inputvals, y_:targetvals})
+  #         writer.add_summary(s, i)
+
+  #       sess.run(train_step, feed_dict={x: inputvals, y_: targetvals})
+
+  #   if save_trained == 1:
+  #       print("Saving neural network to %s.*"%(save_file))
+  #       saver = tf.train.Saver()
+  #       saver.save(sess, save_file)
+  #   #raw_input("Press Enter to continue...")
 
 else: # if we're not training then we must be loading from file to play
 
